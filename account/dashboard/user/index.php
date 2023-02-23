8723ee657a412145f8db4780014c7c94ccabcb69
Your Name <you@example.com>
2023-02-23 10:32:26 +0000

    1st

---
+++ account/dashboard/user/index.php
@@ -0,0 +1,8 @@
+<?php session_start(); ?>
+
+<?php include '../../../components/header.php'; ?>
+  
+  <?php include '../../../components/navigation.php'; ?>
+<h1>User Dashboard</h1>  
+  <?php include '../../../components/footer.php'; ?>
+
